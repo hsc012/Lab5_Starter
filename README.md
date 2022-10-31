fb6f73387ca75ae5bd5c8a7af7c4288fdc80f49a
Sisy Chen <hsc012@ucsd.edu>
2022-10-30 23:57:05 -0700

    expose.js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 5 - Starter
+Name: Hsi Chen
+Link to Continuous Integration: 
+Link to expose.html: 
+Link to explore.html: 
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,46 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  // match images & audios to the corresponding options
+  var selectedHorns = document.getElementById('horn-select');
+  selectedHorns.onchange = function(){
+    var img = document.getElementsByTagName('img')[0];
+    var aud = document.getElementsByClassName('hidden')[0];
+    if(selectedHorns.value == 'air-horn'){
+      img.src = "assets/images/air-horn.svg";
+      aud.src = "assets/audio/air-horn.mp3";
+    }
+    if(selectedHorns.value == 'car-horn'){
+        img.src = "assets/images/car-horn.svg";
+        aud.src = "assets/audio/car-horn.mp3";
+    }
+    if(selectedHorns.value == 'party-horn'){
+        img.src = "assets/images/party-horn.svg";
+        aud.src = "assets/audio/party-horn.mp3";
+    }
+  }
+  // change volume on slider
+  let audio = document.getElementsByClassName('hidden')[0];
+  let volume = document.getElementById('volume-controls');
+  volume.addEventListener('change', function(){
+    var img = document.getElementsByTagName('img')[1];
+    if(volume.value == 0){
+      img.src = "assets.icons/volume-level-0.svg";
+    }
+    if(volume.value >= 1 || volume.value < 33){
+      img.src = "assets/icons/volume-level-1.svg";
+    }
+    if(volume.value >= 33 || volume.value < 67){
+      img.src = "assets/icons/volume-level-2.svg";
+    }
+    if(volume.value >= 67){
+      img.src = "assets/icons/volume-level-3.svg";
+    }
+    audio.volume = volume.value;
+  });
+  // implement the function of the 'Play Sound' button
+  document.getElementsByTagName('button')[0].addEventListener('click', function(){
+    document.getElementsByClassName('hidden')[0].play();
+  });
+
 }
